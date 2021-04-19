8fbb72ff88cb2a5a911671fedb14a17cfe8e3afa
f-diallo <56490509+f-diallo@users.noreply.github.com>
2021-04-19 13:26:17 -0700

    Create initial NextGen project from hero_basic

---
+++ hero_nextgen/Assets/Scripts/CameraSupport.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSupport : MonoBehaviour
+{
+    private Camera mTheCamera;
+    private Bounds mWorldBound; // Computed bound from the camera
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTheCamera = gameObject.GetComponent<Camera>();
+        mWorldBound = new Bounds();
+        float maxY = mTheCamera.orthographicSize;
+        float maxX = mTheCamera.orthographicSize * mTheCamera.aspect;
+        float sizeX = maxX * 2; //include negative/left half of camera
+        float sizeY = maxY * 2;
+        Vector3 c = mTheCamera.transform.position;
+        c.z = 0.0f;
+        mWorldBound.center = c;
+        mWorldBound.size = new Vector3(sizeX, sizeY, 1f);
+        Debug.Log("World Bound: " + mWorldBound);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public Bounds GetWorldBound() { return mWorldBound; }
+
+    public bool isInside(Bounds b1)
+    {
+        return isInsideBounds(b1, mWorldBound);
+    }
+
+    // b1 is inside b2
+    public bool isInsideBounds(Bounds b1, Bounds b2)
+    {
+        return (b1.min.x < b2.max.x) && (b1.max.x > b2.min.x) &&
+                (b1.min.y < b2.max.y) && (b1.max.y > b2.min.y);
+    }
+}
---
+++ hero_nextgen/Assets/Scripts/GameController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private int maxPlanes = 10;
+    private int numberOfPlanes = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Escape)){
+#if UNITY_EDITOR
+            // Application.Quit() does not work in editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+        if(numberOfPlanes < maxPlanes)
+        {
+            //GameObject s = Camera.main.GetComponent<CameraSupport>();
+            //Bounds myBound = GetComponent<Renderer>().bounds;
+            CameraSupport s = Camera.main.GetComponent<CameraSupport>();
+
+            GameObject e = Instantiate(Resources.Load("Prefabs/Enemy") as GameObject); // PreFabs must be in the Resources Folder
+            Vector3 pos = transform.position;
+            pos.x = s.GetWorldBound().min.x + Random.value * s.GetWorldBound().size.x; //Random.value*50;
+            pos.y = s.GetWorldBound().min.y + Random.value * s.GetWorldBound().size.y; //Random.value*50;
+            pos.z = 0;
+            e.transform.localPosition = pos;
+            ++numberOfPlanes;
+        }
+    }
+    
+    public void EnemyDestroyed(){
+        --numberOfPlanes;
+    }
+}
